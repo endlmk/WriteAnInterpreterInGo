b7de136785a88587cae8a188ece8d4078330e8e2
endlmk <1269467+endlmk@users.noreply.github.com>
2020-10-14 23:54:36 +0900

    Impl String()

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/endlmk/monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -30,6 +33,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token // token.LET
 	Name  *Identifier
@@ -38,6 +51,21 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type RetruenStatement struct {
 	Token       token.Token // 'return' token
@@ -46,6 +74,33 @@ type RetruenStatement struct {
 
 func (rs *RetruenStatement) statementNode()       {}
 func (rs *RetruenStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *RetruenStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // Token of first elem of expr
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
 
 type Identifier struct {
 	Token token.Token // token.IDENT
@@ -54,3 +109,4 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
