2977fce90a47e02c89499067e13cb91ab2d64ee6
endlmk <1269467+endlmk@users.noreply.github.com>
2021-01-10 22:51:25 +0900

    Impl builtin puts.

--- evaluator/bultins.go
+++ evaluator/bultins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/endlmk/monkey/object"
+import (
+	"fmt"
+
+	"github.com/endlmk/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -95,6 +99,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
